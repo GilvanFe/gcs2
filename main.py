2c8256a0b2fefa77e6a73ff318da53c10c04266f
Giovanni Martinovsky <giovannimartinovsky@gmail.com>
2023-05-15 14:36:15 -0400

    feat: função de soma
    Adicionei uma função que soma os 3 inteiros de entrada. Mantive a função de multiplicação, alterando apenas seu nome de "resultado" para "res_mult".

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ b = 3
 c = 2
 
 # Multiplicando as variáveis
-resultado = a * b * c
+res_multi = a * b * c
+res_add = a + b + c
 
 # Imprimindo o resultado
-print(resultado)
+print(res_mult)
+print(res_add)
